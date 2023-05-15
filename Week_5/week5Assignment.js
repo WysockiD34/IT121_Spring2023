11e7ce39fea5ba22216a88100c3b7e0f28dd300d
WysockiD34 <114697378+WysockiD34@users.noreply.github.com>
2023-05-15 00:38:32 +0000

    worked on in vscode

---
+++ Week_5/week5Assignment.js
@@ -0,0 +1,24 @@
+const grid = [];
+const rows = 9; //sets the number of rows to 9
+const columns = 9; //sets number of columns in grid to 9
+
+for (let i = 0; i <= rows; i++) { //this creates a loop that starts at 0 and ends when i is less than or equal to the number of rows
+  const row = [];
+
+  for (let j = 0; j <= columns; j++) { //this creates a loop that starts at 0 and ends when j is less than or equal to the number of rows
+    const amount = i * j;
+    row.push(amount);
+  }
+
+  grid.push(row);
+}
+
+console.table(grid); //this displays the grid in the console!!
+
+//code above is the Assignment 1 of Week 5 :)
+
+for (let i = 1; i <= 12; i++) { //creates a loop that will run until i is equal or less than to 12
+    const amount = 12 * i; 
+    console.log("12 * " + i + " = " + amount);
+}
+//code  above is assignment 2 of week 5
